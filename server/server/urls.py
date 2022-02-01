68c3829179b76c6804275bee39713e709932e6f2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-01 19:51:40 +0530

    :railway_track: Updating api urls in server

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('user.urls')),
+    path('api/v1/', include('user.urls')),
 ]
