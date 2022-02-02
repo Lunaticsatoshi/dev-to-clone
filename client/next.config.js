74d3a15a1e5b72387febdb0be0cc3c134823abdd
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-02 21:01:55 +0530

    :wrench: Adding eslint config to next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	eslint: {
+		dirs: ["pages", "utils", "components", "store", "hooks"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
