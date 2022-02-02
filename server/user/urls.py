f22aa445f7d8153dbd0ba42628dc016232a0123f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-02 20:39:50 +0530

    :railway_track: Adding verify email view to url route

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, VerifyEmailView
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='user-register'),
+    path('email-verify/', VerifyEmailView.as_view(), name='email-verify'),
 ]
