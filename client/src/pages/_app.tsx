8519c676c89ba60e07c3088f4625ddf33058f39b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-03 20:49:34 +0530

    :zip: Adding next theme provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "src/styles/app.scss";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider defaultTheme="system">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
