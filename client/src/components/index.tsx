a599a6948ebcfc62551492864b8cb0ec62d09f84
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-03 21:05:31 +0530

    :zap: Adding feed component to common export

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,5 +1,6 @@
 import Layout from "./shared/Layout";
 import Sidebar from "./navigation/Sidebar";
 import ListingBar from "./navigation/ListingBar";
+import Feed from "./feed/Feed";
 
-export { Layout, Sidebar, ListingBar };
+export { Layout, Sidebar, ListingBar, Feed };
