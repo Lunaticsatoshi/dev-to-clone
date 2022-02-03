92da0a2e47ce67ce07eb95e9bbaeaa1f53499774
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-03 21:35:19 +0530

    :art: Updating header component

--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -1,5 +1,42 @@
+import Link from "next/link";
+import { FaDev } from "react-icons/fa";
+import { BiMessageRoundedCheck } from "react-icons/bi";
+import { RiNotificationLine } from "react-icons/ri";
+import { FiSearch } from "react-icons/fi";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="header-container-left">
+          <Link href="/" passHref>
+            <div className="header-container-left__logo">
+              <FaDev size="3.125rem" />
+            </div>
+          </Link>
+
+          <div className="header-container-left__searchBox">
+            <form>
+              <input type="text" placeholder="Search..." aria-label="search" />
+            </form>
+          </div>
+        </div>
+
+        <div className="header-container-right">
+          <button className="post-button">Write a post</button>
+          <i className="hidden-search">
+            <FiSearch />
+          </i>
+          <i>
+            <BiMessageRoundedCheck />
+          </i>
+          <i>
+            <RiNotificationLine />
+          </i>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
