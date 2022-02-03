2c89f539b7cdfa550f12ca69ae1044f1f849d2a6
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-03 21:15:15 +0530

    :package: Adding feed component to home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 
-import { Layout } from "src/components";
+import { Layout, Sidebar, ListingBar, Feed } from "src/components";
 
 const Home: NextPage = () => {
   return (
     <>
       <Layout title="Home">
-        <h1 className="text-3xl font-bold">Welcome to Dev.to clone</h1>
+        <Sidebar />
+        <Feed />
+        <ListingBar />
       </Layout>
     </>
   );
