d303796ecc31f8ae1a80d0f1170df140dd78b29a
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-04 21:31:52 +0530

    :railway_track: Adding login view to urls

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmailView
+from .views import RegisterView, VerifyEmailView, LoginView
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='user-register'),
+    path('auth/login/', LoginView.as_view(), name='user-login'),
     path('email-verify/', VerifyEmailView.as_view(), name='email-verify'),
 ]
