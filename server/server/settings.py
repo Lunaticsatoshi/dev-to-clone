d90862d4a1efae1ad1c436cfd754db7ed758b464
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-04 20:40:28 +0530

    :wrench: Adding sendgrid to config to settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -168,7 +168,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
