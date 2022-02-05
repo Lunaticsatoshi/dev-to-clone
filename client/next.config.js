52a04243f80dbfefcaaf77f0b895fbb4756bb3c7
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-05 21:17:53 +0530

    :wrench: Formattednext config

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	eslint: {
-		dirs: ["pages", "utils", "components", "store", "hooks"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-	},
+  reactStrictMode: true,
+  eslint: {
+    dirs: ["pages", "utils", "components", "store", "hooks"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 };
 
 module.exports = nextConfig;
