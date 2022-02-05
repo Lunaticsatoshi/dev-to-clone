99ded694aefcfe449b34cdc3d6befde0a0ce08fe
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-05 21:16:42 +0530

    :art: Adding components to common export

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -2,5 +2,7 @@ import Layout from "./shared/Layout";
 import Sidebar from "./navigation/Sidebar";
 import ListingBar from "./navigation/ListingBar";
 import Feed from "./feed/Feed";
+import Button from "./ui/Button";
+import SearchBox from "./ui/SearchBox";
 
-export { Layout, Sidebar, ListingBar, Feed };
+export { Layout, Sidebar, ListingBar, Feed, Button, SearchBox };
