8ca3baa4e3812f8c2595bd98ec76177ab13486f7
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-06 22:02:33 +0530

    :art: Adding dropdown component to common export

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -5,5 +5,7 @@ import Feed from "./feed/Feed";
 import Button from "./ui/Button";
 import SearchBox from "./ui/SearchBox";
 import ThemeToggle from "./ui/ThemeToggle";
+import DropDown from "./ui/Dropdown/DropDown";
+import DropDownItem from "./ui/Dropdown/DropDownItem";
 
-export { Layout, Sidebar, ListingBar, Feed, Button, SearchBox, ThemeToggle };
+export { Layout, Sidebar, ListingBar, Feed, Button, SearchBox, ThemeToggle, DropDown, DropDownItem };
