65dce10b9cbcfd38475bdb6b820332e29a6dde07
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-06 18:59:22 +0530

    :technologist: Adding swagger documentation to api

--- server/server/settings.py
+++ server/server/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_yasg',
     'user',
 ]
 
--- server/server/urls.py
+++ server/server/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Dev.to Clone API",
+      default_version='v1',
+      description="A Django backend api for dev to clone",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="deysarkersudeep@gmail.com"),
+      license=openapi.License(name="GPL License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/v1/', include('api.urls')),
 ]
