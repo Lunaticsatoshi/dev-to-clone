c0478eda0143c75dcc1e32771652dad9b0aea288
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-06 17:45:13 +0530

    :wrench: Adding smpt backend config to settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -167,6 +167,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
