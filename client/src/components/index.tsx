92771c3ac03f60e8062e98ee1a400220d492a0c6
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-07 21:23:37 +0530

    :package: Adding components to common export

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,6 +1,8 @@
 import Layout from "./shared/Layout";
 import Sidebar from "./navigation/Sidebar";
-import ListingBar from "./navigation/ListingBar";
+import SidebarResponsive from "./navigation/SidebarResposnive";
+import SidebarItem from "./navigation/SidebarItem";
+import ListingBar from "./listing/ListingBar";
 import Feed from "./feed/Feed";
 import Button from "./ui/Button";
 import SearchBox from "./ui/SearchBox";
@@ -8,4 +10,16 @@ import ThemeToggle from "./ui/ThemeToggle";
 import DropDown from "./ui/Dropdown/DropDown";
 import DropDownItem from "./ui/Dropdown/DropDownItem";
 
-export { Layout, Sidebar, ListingBar, Feed, Button, SearchBox, ThemeToggle, DropDown, DropDownItem };
+export {
+  Layout,
+  Sidebar,
+  ListingBar,
+  Feed,
+  Button,
+  SearchBox,
+  ThemeToggle,
+  DropDown,
+  DropDownItem,
+  SidebarResponsive,
+  SidebarItem,
+};
