c0775529c689613c4cc1a1eae6dad4da58376566
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-08 20:06:55 +0530

    :railway_track: Adding token refresh view to urls

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import RegisterView, VerifyEmailView, LoginView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='user-register'),
     path('auth/login/', LoginView.as_view(), name='user-login'),
     path('email-verify/', VerifyEmailView.as_view(), name='email-verify'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
