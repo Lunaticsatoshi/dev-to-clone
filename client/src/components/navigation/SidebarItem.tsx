6a57df3f127304fb32439ada45b1327f32069e4c
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-08 21:07:53 +0530

    :art: Removing key from sidebar items props

--- client/src/components/navigation/SidebarItem.tsx
+++ client/src/components/navigation/SidebarItem.tsx
@@ -4,17 +4,15 @@ type SidebarItemsProps = {
   children: ReactNode | string | number;
   onClick?: () => void;
   className?: string;
-  key?: string | number;
 };
 
 const SidebarItems: FC<SidebarItemsProps> = ({
   children,
   onClick,
   className,
-  key,
 }) => {
   return (
-    <li className={className} key={key} onClick={onClick}>
+    <li className={className} onClick={onClick}>
       {children}
     </li>
   );
