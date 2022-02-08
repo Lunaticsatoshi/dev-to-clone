d7da6aac34584e0902628483491d1caef7ae6ccf
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-08 21:49:34 +0530

    :desktop_computer: Adding Dropdown to component level

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -7,8 +7,8 @@ import Feed from "./feed/Feed";
 import Button from "./ui/Button";
 import SearchBox from "./ui/SearchBox";
 import ThemeToggle from "./ui/ThemeToggle";
-import DropDown from "./ui/Dropdown/DropDown";
-import DropDownItem from "./ui/Dropdown/DropDownItem";
+import DropDown from "./Dropdown/DropDown";
+import DropDownItem from "./Dropdown/DropDownItem";
 
 export {
   Layout,
