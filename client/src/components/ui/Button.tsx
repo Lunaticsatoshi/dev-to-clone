d221c6116fbc6362fc09993ff4137c1ea1380439
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-09 21:47:47 +0530

    :art: Adding new props to button component

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,13 +1,30 @@
 import { FC, ReactNode } from "react";
 
 type ButtonProps = {
-    children: ReactNode | string | number;
-    onClick?: () => void;
-    className?: string;
+  children: ReactNode | string | number;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
 };
 
-const Button: FC<ButtonProps> = ({ children, onClick, className }) => {
-  return <button className={className} onClick={onClick}>{children}</button>;
+const Button: FC<ButtonProps> = ({
+  children,
+  onClick,
+  className,
+  type,
+  disabled,
+}) => {
+  return (
+    <button
+      className={className}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
