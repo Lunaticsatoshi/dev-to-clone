da7c2fbb393d1189276d487bebed925a875c15c0
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-10 20:30:29 +0530

    :pencil2: Updating responses for user serializers

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -30,11 +30,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         username = attrs.get('username', '')
 
         if not username.isalnum():
-            raise serializers.ValidationError({'username': self.default_error_messages['username']}, code=status.HTTP_400_BAD_REQUEST)
+            raise serializers.ValidationError({'status': False, 'message': self.default_error_messages['username']}, code=status.HTTP_400_BAD_REQUEST)
         if CustomUser.objects.filter(email=email).exists():
-            raise serializers.ValidationError({'email': self.default_error_messages['email_exists']}, code=status.HTTP_400_BAD_REQUEST)
+            raise serializers.ValidationError({'status': False, 'message': self.default_error_messages['email_exists']}, code=status.HTTP_400_BAD_REQUEST)
         if CustomUser.objects.filter(username__iexact=username).exists():
-            raise serializers.ValidationError({'username': self.default_error_messages['username_exists']}, code=status.HTTP_400_BAD_REQUEST)
+            raise serializers.ValidationError({'status': False, 'message': self.default_error_messages['username_exists']}, code=status.HTTP_400_BAD_REQUEST)
         return attrs
 
     def create(self, validated_data):
