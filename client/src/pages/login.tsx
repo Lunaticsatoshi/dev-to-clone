a5aafef7fd2430277b5343c73c4e9ea9359d2bdc
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-10 22:12:13 +0530

    :art: Updating login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -10,15 +10,17 @@ const Login: NextPage = () => {
         <div className="auth-container">
           <div className="auth-container__content registration flex flex-col justify-between items-center">
             <div className="header flex flex-col justify-center items-center text-center">
-              <h1 className="header__title text-3xl font-bold">
+              <h1 className="title text-3xl font-bold">
                 Welcome to Dev community
               </h1>
-              <div className="header__description text-base">
+              <div className="description text-base">
                 DEV Community is a community of 795,483 amazing developers
               </div>
             </div>
-            <div className="auth-form">
-              <LoginForm message="Login" />
+            <div className="w-full actions mt-2">
+              <div className="w-full actions-email mt-2">
+                <LoginForm message="Have a password? Login with your email address" />
+              </div>
             </div>
           </div>
         </div>
