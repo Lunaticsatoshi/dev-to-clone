74e935cf0949fffefa58cfc2d93c76a1c926d8a3
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-11 20:49:12 +0530

    :railway_track: Adding profile update view to urls

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmailView, LoginView, get_users, get_users_by_interest, get_user
+from .views import RegisterView, VerifyEmailView, LoginView, ProfileUpdateView, get_users, get_users_by_interest, get_user
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -11,5 +11,6 @@ urlpatterns = [
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('all/', get_users, name='get-users'),
     path('<str:username>/', get_user, name='get-user'),
-    path('interests/<str:interest>', get_users_by_interest, name='get-users-by-interest')
+    path('interests/<str:interest>', get_users_by_interest, name='get-users-by-interest'),
+    path('profile/update', ProfileUpdateView.as_view(), name='profile-update')
 ]
