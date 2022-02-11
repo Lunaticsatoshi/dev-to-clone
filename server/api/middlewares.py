597022f720e0d6f1511a72e31fb6faa4d3888c72
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-11 21:22:27 +0530

    :jigsaw: Adding custom exception handler as a middleware

---
+++ server/api/middlewares.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import exception_handler
+from rest_framework.exceptions import NotAuthenticated
+
+def custom_exception_handler(exc, context):
+    if isinstance(exc, NotAuthenticated):
+        return Response({ 'status': False, 'message': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    return exception_handler(exc, context)
