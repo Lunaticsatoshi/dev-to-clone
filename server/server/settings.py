c3dc4d2bbd4c6106067ea81e378f098058694c9f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-11 21:22:50 +0530

    :wrench: Adding custom exception handler to settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -103,6 +103,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
+    'EXCEPTION_HANDLER': 'api.middlewares.custom_exception_handler',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
