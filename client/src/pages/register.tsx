6bfca5141778b149038606e1227ac87b822089d2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-12 22:23:19 +0530

    :art: Adding register form to register page

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from "next";
+
+import { RegisterForm } from "src/modules";
+import { Layout } from "src/components";
+
+const RegisterPage: NextPage = () => {
+  return (
+    <>
+      <Layout title="Register">
+        <div className="auth-container">
+          <div className="auth-container__content registration flex flex-col justify-between items-center">
+            <div className="header flex flex-col justify-center items-center text-center">
+              <h1 className="title text-3xl font-bold">
+                Welcome to Dev community
+              </h1>
+              <div className="description text-base">
+                DEV Community is a community of 795,483 amazing developers
+              </div>
+            </div>
+            <div className="w-full actions mt-2">
+              <div className="w-full actions-email mt-2">
+                <RegisterForm message="Already have an account?" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default RegisterPage;
