d0856782a1d542a217a46e0be16b1839c28b68fa
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-13 22:04:59 +0530

    :alembic: Api testing

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { axiosInstance } from "src/utils";
 import type { NextPage } from "next";
 
 import { LeftSidebar } from "src/modules";
 import { Layout, ListingBar, Feed } from "src/components";
 
 const HomePage: NextPage = () => {
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axiosInstance.get("/user/all/");
+      console.log(response);
+    };
+
+    getData();
+  }, []);
   return (
     <>
       <Layout title="Home">
--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -2,49 +2,86 @@ import axios, { AxiosInstance } from "axios";
 import jwt_decode from "jwt-decode";
 import dayjs from "dayjs";
 
-const baseURL = "http://localhost:8000/api/v1";
+const baseURL = "http://127.0.0.1:8000/api/v1";
 
 type authToken = {
   access: string;
   refresh: string;
 };
+
+type authHeader = {Authorization: string};
 // eslint-disable-next-line init-declarations
 let authTokens: authToken | null;
 // eslint-disable-next-line init-declarations
 let axiosInstance: AxiosInstance;
 
-if (typeof window !== "undefined") {
-  authTokens = localStorage.getItem("authTokens")
-    ? JSON.parse(localStorage.getItem("authTokens")!)
-    : null;
+const getToken = async (): Promise<authToken | null> => {
+    const response = await axios.post(`${baseURL}/user/auth/token/refresh/`, {
+      refresh: authTokens!.refresh,
+    });
 
-  axiosInstance = axios.create({
-    baseURL,
-    headers: { Authorization: `Bearer ${authTokens?.access}` || "" },
-  });
+    return response.data;
+  };
 
-  axiosInstance?.interceptors.request.use(async (req) => {
+  const getAuthHeaders = async (): Promise<Partial<authHeader>> => {
+    authTokens = localStorage.getItem("authTokens")
+      ? JSON.parse(localStorage.getItem("authTokens")!)
+      : null;
+
     if (!authTokens) {
-      authTokens = localStorage.getItem("authTokens")
-        ? JSON.parse(localStorage.getItem("authTokens")!)
-        : null;
-
-      req.headers!.Authorization = `Bearer ${authTokens?.access}` || "";
+      return {};
     }
 
     const user: any = jwt_decode(authTokens?.access || "");
     const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
 
-    if (!isExpired) return req;
+    if (!isExpired) return { Authorization: `Bearer ${authTokens?.access}` };
 
-    const response = await axios.post(`${baseURL}/user/auth/token/refresh/`, {
-      refresh: authTokens!.refresh,
+    const refreshedTokens = await getToken();
+
+    localStorage.setItem("authTokens", JSON.stringify(refreshedTokens!));
+
+    return { Authorization: `Bearer ${refreshedTokens!.access}` };
+  };
+
+  if (typeof window !== "undefined") {
+    const authHeader = getAuthHeaders();
+    axiosInstance = axios.create({
+      baseURL,
     });
+  }
 
-    localStorage.setItem("authTokens", JSON.stringify(response.data));
-    req.headers!.Authorization = `Bearer ${response.data.access}`;
-    return req;
-  });
-}
+// if (typeof window !== "undefined") {
+//   authTokens = localStorage.getItem("authTokens")
+//     ? JSON.parse(localStorage.getItem("authTokens")!)
+//     : null;
+
+//   axiosInstance = axios.create({
+//     baseURL,
+//   });
+
+//   axiosInstance?.interceptors.request.use((req) => {
+//     if (!authTokens) {
+//       authTokens = localStorage.getItem("authTokens")
+//         ? JSON.parse(localStorage.getItem("authTokens")!)
+//         : null;
+
+//         req.headers!.Authorization = `Bearer ${authTokens?.access}`;
+//     }
+
+//     const user: any = jwt_decode(authTokens?.access || "");
+//     const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+
+//     if (!isExpired) return req;
+
+//     const response = await axios.post(`${baseURL}/user/auth/token/refresh/`, {
+//       refresh: authTokens!.refresh,
+//     });
+
+//     localStorage.setItem("authTokens", JSON.stringify(response.data));
+//     req.headers!.Authorization = `Bearer ${response.data.access}`;
+//     return req;
+//   });
+// }
 
 export default axiosInstance!;
