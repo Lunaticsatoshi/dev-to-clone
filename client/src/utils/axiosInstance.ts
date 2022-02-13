6b15b5bac430075d03536093132949599c911f9e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-13 20:20:12 +0530

    :sparkles: Implementing axios instance as a utility

---
+++ client/src/utils/axiosInstance.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import dayjs from "dayjs";
+
+const baseURL = "http://127.0.0.1:8000/api/v1";
+
+type authToken = {
+  access: string;
+  refresh: string;
+};
+
+let authTokens: authToken | null = localStorage.getItem("authTokens")
+  ? JSON.parse(localStorage.getItem("authTokens")!)
+  : null;
+
+const axiosInstance = axios.create({
+  baseURL,
+  headers: { Authorization: `Bearer ${authTokens?.access}` },
+});
+
+axiosInstance.interceptors.request.use(async (req) => {
+  if (!authTokens) {
+    authTokens = localStorage.getItem("authTokens")
+      ? JSON.parse(localStorage.getItem("authTokens")!)
+      : null;
+
+    req.headers!.Authorization = `Bearer ${authTokens?.access}`;
+  }
+
+  const user: any = jwt_decode(authTokens!.access);
+  const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+
+  if (!isExpired) return req;
+
+  const response = await axios.post(`${baseURL}/user/auth/token/refresh/`, {
+    refresh: authTokens!.refresh,
+  });
+
+  localStorage.setItem("authTokens", JSON.stringify(response.data));
+  req.headers!.Authorization = `Bearer ${response.data.access}`;
+  return req;
+});
+
+export default axiosInstance;
