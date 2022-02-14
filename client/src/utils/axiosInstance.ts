61ae95011109ea89b9057f65cc91123376bde687
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-14 20:36:16 +0530

    :ambulance: Fixing axios instance to work with types

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -15,14 +15,6 @@ let authTokens: authToken | null;
 // eslint-disable-next-line init-declarations
 let axiosInstance: AxiosInstance;
 
-const getToken = async (): Promise<authToken | null> => {
-  const response = await axios.post(`${baseURL}/user/auth/token/refresh/`, {
-    refresh: authTokens!.refresh,
-  });
-
-  return response.data;
-};
-
 const getAuthHeaders = async (): Promise<Partial<authHeader>> => {
   authTokens = localStorage.getItem("authTokens")
     ? JSON.parse(localStorage.getItem("authTokens")!)
@@ -30,64 +22,37 @@ const getAuthHeaders = async (): Promise<Partial<authHeader>> => {
 
   if (!authTokens) {
     return {};
+  } else {
+    const user: any = jwt_decode(authTokens?.access || "");
+    const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+
+    if (!isExpired) return { Authorization: `Bearer ${authTokens!.access}` };
+
+    const refreshedTokens = await axios.post(
+      `${baseURL}/user/auth/token/refresh/`,
+      {
+        refresh: authTokens!.refresh,
+      },
+    );
+
+    localStorage.setItem("authTokens", JSON.stringify(refreshedTokens!));
+
+    return { Authorization: `Bearer ${authTokens!.access}` };
   }
-
-//   const user: any = jwt_decode(authTokens?.access || "");
-//   const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
-
-//   if (!isExpired) return { Authorization: `Bearer ${authTokens!.access}` };
-
-  const refreshedTokens = await getToken();
-
-//   localStorage.setItem("authTokens", JSON.stringify(refreshedTokens!));
-
-  return { Authorization: `Bearer ${authTokens!.access}` };
 };
 
 if (typeof window !== "undefined") {
   axiosInstance = axios.create({
     baseURL,
+    headers: {}
   });
 
   axiosInstance.interceptors.request.use(async (req) => {
     const authHeader = await getAuthHeaders();
-    console.log(authHeader);
-
     req.headers = { ...authHeader };
+
+    return req;
   });
 }
-
-// if (typeof window !== "undefined") {
-//   authTokens = localStorage.getItem("authTokens")
-//     ? JSON.parse(localStorage.getItem("authTokens")!)
-//     : null;
-
-//   axiosInstance = axios.create({
-//     baseURL,
-//   });
-
-//   axiosInstance?.interceptors.request.use((req) => {
-//     if (!authTokens) {
-//       authTokens = localStorage.getItem("authTokens")
-//         ? JSON.parse(localStorage.getItem("authTokens")!)
-//         : null;
-
-//         req.headers!.Authorization = `Bearer ${authTokens?.access}`;
-//     }
-
-//     const user: any = jwt_decode(authTokens?.access || "");
-//     const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
-
-//     if (!isExpired) return req;
-
-//     const response = await axios.post(`${baseURL}/user/auth/token/refresh/`, {
-//       refresh: authTokens!.refresh,
-//     });
-
-//     localStorage.setItem("authTokens", JSON.stringify(response.data));
-//     req.headers!.Authorization = `Bearer ${response.data.access}`;
-//     return req;
-//   });
-// }
 
 export default axiosInstance!;
