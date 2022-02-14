fe6192b0f88b6e733a18cc0e7c9af0f90d709d8a
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-14 21:10:46 +0530

    :zap: Adding auth context and auth context provider

---
+++ client/src/contexts/AuthContext.tsx
@@ -0,0 +1,38 @@
+import React, {
+  FC,
+  Dispatch,
+  ReactNode,
+  createContext,
+  useReducer,
+} from "react";
+
+import { authReducer, AuthAction, AuthState } from "src/reducers";
+
+type Props = {
+  children: ReactNode;
+};
+
+const initialState = {
+  user: {},
+  profile: {},
+  isAuthenticated: false,
+  error: "",
+  redirectRoute: "",
+};
+
+export const AuthContext = createContext<{
+  state: AuthState;
+  dispatch: Dispatch<AuthAction>;
+}>({
+  state: initialState,
+  dispatch: () => null,
+});
+
+export const AuthContextProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
