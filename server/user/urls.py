98d6e7a91e045e0dfd37bcb3ffb93d3344aef389
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-15 20:03:32 +0530

    :railway_track: Updating profile url

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmailView, LoginView, ProfileUpdateView, get_users, get_users_by_interest, get_user, get_profile
+from .views import RegisterView, VerifyEmailView, LoginView, ProfileUpdateView, get_users, get_users_by_interest, get_user, get_current_user
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -12,6 +12,6 @@ urlpatterns = [
     path('all/', get_users, name='get-users'),
     path('<str:username>/', get_user, name='get-user'),
     path('interests/<str:interest>/', get_users_by_interest, name='get-users-by-interest'),
-    path('profile/', get_profile, name='get-profile'),
+    path('profile/current/', get_current_user, name='get-profile'),
     path('profile/update/', ProfileUpdateView.as_view(), name='profile-update'),
 ]
