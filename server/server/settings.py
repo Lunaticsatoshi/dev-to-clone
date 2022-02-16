dcdde762bb77e67d7532e65cc4e1ef2f76e72924
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-16 21:05:01 +0530

    :wrench: Updating jwt access token exp in settings config

--- server/server/settings.py
+++ server/server/settings.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
