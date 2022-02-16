88052a0ab529d987e9a55f85ea3ef231efe1981e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-16 20:38:08 +0530

    :railway_track: Adding get user, follow user and logout to urls

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmailView, LoginView, ProfileUpdateView, get_users, get_users_by_interest, get_user, get_current_user
+from .views import RegisterView, VerifyEmailView, LoginView, ProfileUpdateView, get_users, get_users_by_interest, get_user, get_current_user, follow_user, add_interests, logout
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='user-register'),
     path('auth/login/', LoginView.as_view(), name='user-login'),
+    path('auth/logout/', logout, name='user-logout'),
     path('email-verify/', VerifyEmailView.as_view(), name='email-verify'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('all/', get_users, name='get-users'),
@@ -14,4 +15,6 @@ urlpatterns = [
     path('interests/<str:interest>/', get_users_by_interest, name='get-users-by-interest'),
     path('profile/current/', get_current_user, name='get-profile'),
     path('profile/update/', ProfileUpdateView.as_view(), name='profile-update'),
+    path('profile/interests/add/', add_interests, name='add-interests'),
+    path('profile/<str:username>/follow', follow_user, name='follow-user'),
 ]
