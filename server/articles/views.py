8c3ecf18f0846800f9f4e295a50d9846fab5b40e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-17 20:21:28 +0530

    :gear: Seperating article create to a seperate view

--- server/articles/views.py
+++ server/articles/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 
 from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import ArticleSerializer
-from .models import Article
+from .models import Article, HashTag
 from .utils import Utils
 from .permissions import IsOwner
 
 # Create your views here.
-class UserArticleListApiView(ListCreateAPIView):
+class UserArticleListApiView(ListAPIView):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    permission_classes = (IsAuthenticated,)
+    
+    def get_queryset(self):
+        user = self.request.user
+        return self.queryset.filter(user=user).order_by('-created_at')
+    
+class UserArticleCreateAPIView(CreateAPIView):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
     permission_classes = (IsAuthenticated,)
@@ -21,17 +30,26 @@ class UserArticleListApiView(ListCreateAPIView):
         user = self.request.user
         data = self.request.data
         title = data.get('title')
+        content = data.get('content')
+        tags = data.get('tags')
+        status = data.get('status')
+        try:
+            if not title or not content:
+                raise Exception('Title or Content is required')
+            
+            slug = Utils.generate_slug(title)
+            
+            article = Article.objects.create(title=title, content=content, slug=slug, user=user, status=status)
+            
+            if tags is not None:
+                article.tags.set(HashTag.objects.get_or_create(tags=tag_name)[0] for tag_name in tags)
+                
+            article.save()
+            serializer = ArticleSerializer(article, many=False)
+            return Response({ 'message': 'Article created successfully', 'article': serializer.data }, status=status.HTTP_201_CREATED)
         
-        if not title:
-            raise Exception('Title is required')
-        
-        slug = Utils.generate_slug(title)
-        
-        return serializer.save(user=user, slug=slug)
-    
-    def get_queryset(self):
-        user = self.request.user
-        return self.queryset.filter(user=user)
+        except Exception as e:
+            return Response({ 'message': str(e) }, status=status.HTTP_400_BAD_REQUEST)
     
 class UserArticleDetailApiView(RetrieveUpdateDestroyAPIView):
     serializer_class = ArticleSerializer
@@ -41,7 +59,7 @@ class UserArticleDetailApiView(RetrieveUpdateDestroyAPIView):
     
     def get_queryset(self):
         user = self.request.user
-        return super().queryset.filter(user=user)
+        return self.queryset.filter(user=user)
     
     
 @api_view(['GET'])
