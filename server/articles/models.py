b25ed4db01b3bdc6a171b038cda5d7156b7cb761
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-17 19:47:35 +0530

    :bricks: Creating hashtag model

--- server/articles/models.py
+++ server/articles/models.py
@@ -6,6 +6,12 @@ from api.models import BaseModel
 from user.models import CustomUser
 
 # Create your models here.
+
+class HashTag(BaseModel):
+    tag = models.CharField(max_length=500, db_index=True)
+    
+    def __str__(self) -> str:
+        return f"{self.id} | {self.tag}"
 class Article(BaseModel):
     id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
     user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
@@ -15,6 +21,7 @@ class Article(BaseModel):
     slug = models.SlugField(max_length=700, unique=True, db_index=True)
     clap_count = models.IntegerField(null=True, default=0)
     comment_count = models.IntegerField(null=True, default=0)
+    tags=models.ManyToManyField(HashTag, related_name='article_tags', blank=True)
     
     def __str__(self) -> str:
         return f"{self.id} | {self.title}"
