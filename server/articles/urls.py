f061ad44a20280e64538411db21c0c3e1324006f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-17 21:05:39 +0530

    :railway_track: Adding clap article and  create edit and delete article comment view to urls

--- server/articles/urls.py
+++ server/articles/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import UserArticleListApiView, UserArticleCreateAPIView, UserArticleDetailApiView, get_article, get_articles
+from .views import UserArticleListApiView, UserArticleCreateAPIView, UserArticleDetailApiView, get_article, get_articles, add_clap, add_comment, edit_comment, delete_comment
 
 urlpatterns = [
     path('user/all', UserArticleListApiView.as_view(), name='user-article-list'),
     path('user/create', UserArticleCreateAPIView.as_view(), name='user-article-create'),
     path('user/<int:id>', UserArticleDetailApiView.as_view(), name='user-article-detail'),
     path('all/', get_articles, name='all-articles'),
+    path('clap/', add_clap, name='add-clap'),
+    path('comment/', add_comment, name='add-comment'),
     path('<str:slug>/', get_article, name='article-detail'),
+    path('comment/<str:pk>/edit', edit_comment, name='edit-comment'),
+    path('comment/<str:pk>/delete', delete_comment, name='delete-comment'),
 ]
