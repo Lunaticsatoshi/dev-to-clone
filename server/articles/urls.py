fe8920cb1c59535c6a353bdf108603adb42f932b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-17 20:22:11 +0530

    :railway_track: Adding create article view to urls

--- server/articles/urls.py
+++ server/articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserArticleListApiView, UserArticleDetailApiView, get_article, get_articles
+from .views import UserArticleListApiView, UserArticleCreateAPIView, UserArticleDetailApiView, get_article, get_articles
 
 urlpatterns = [
     path('user/all', UserArticleListApiView.as_view(), name='user-article-list'),
+    path('user/create', UserArticleCreateAPIView.as_view(), name='user-article-create'),
     path('user/<int:id>', UserArticleDetailApiView.as_view(), name='user-article-detail'),
     path('all/', get_articles, name='all-articles'),
     path('<str:slug>/', get_article, name='article-detail'),
