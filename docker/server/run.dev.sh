c9e8a94f792ef3e6861d55969e19167014ced6b1
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-18 20:26:11 +0530

    :hammer: Adding initadmin scripts

--- docker/server/run.dev.sh
+++ docker/server/run.dev.sh
@@ -3,5 +3,8 @@
 echo "==> $(date +%H:%M:%S) ==> Migrating Django models... "
 python manage.py migrate --noinput
 
+echo "==> $(date +%H:%M:%S) ==> Initializing Django admin account.. "
+python manage.py initadmin
+
 echo "==> $(date +%H:%M:%S) ==> Running server... "
 python manage.py runserver 0.0.0.0:8000
--- docker/server/run.prod.sh
+++ docker/server/run.prod.sh
@@ -3,6 +3,9 @@
 echo "==> $(date +%H:%M:%S) ==> Migrating Django models... "
 python manage.py migrate --noinput
 
+echo "==> $(date +%H:%M:%S) ==> Initializing Django admin account.. "
+python manage.py initadmin
+
 echo "==> $(date +%H:%M:%S) ==> Collecting statics... "
 rm -rf /static/*
 cp -r staticfiles/ /static/
