dfe7b61bdcc26bc365b7848aaea9d715bd73644d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-18 20:22:35 +0530

    :zap: Automatically add superuser

---
+++ server/user/management/commands/initadmin.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from user.models import CustomUser
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not CustomUser.objects.filter(email=settings.ADMIN_EMAIL).exists():
+            CustomUser.objects.create_superuser(
+                email=settings.ADMIN_EMAIL,
+                password=settings.ADMIN_PASSWORD,
+            )
+            print('Admin account created successfully')
+        else:
+            print('Admin account already exists')
