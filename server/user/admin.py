72265e7d6fa7bfb6f53c10d67ffa68bac9b774af
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-19 19:35:17 +0530

    :wrench: Registering user profile and interests to admin

--- server/user/admin.py
+++ server/user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import CustomUser, UserProfile, Interests
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(UserProfile)
+
+admin.site.register(Interests)
