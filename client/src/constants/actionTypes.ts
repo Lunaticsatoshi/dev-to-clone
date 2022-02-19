dd5d375902be2d05b00006facd1064b013dadaf6
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-19 20:57:20 +0530

    :zap: Updating action types constants

--- client/src/constants/actionTypes.ts
+++ client/src/constants/actionTypes.ts
@@ -2,9 +2,7 @@ export const OPEN_SIDEBAR = "OPEN_SIDEBAR";
 export const CLOSE_SIDEBAR = "CLOSE_SIDEBAR";
 
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILURE = "LOGIN_FAILURE";
-export const LOGOUT = "LOGOUT";
-export const SET_USER = "SET_USER";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
-export const REGISTER_FAILURE = "REGISTER_FAILURE";
+export const SET_USER = "SET_USER";
 export const GET_ALL_USERS = "GET_ALL_USERS";
+export const LOGOUT = "LOGOUT";
