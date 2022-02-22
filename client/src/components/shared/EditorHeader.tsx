468615bbf1d2347358291006953e7a3ff222d01a
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-22 22:01:42 +0530

    :art: Updating editor header

--- client/src/components/shared/EditorHeader.tsx
+++ client/src/components/shared/EditorHeader.tsx
@@ -1,33 +1,26 @@
 import { useState } from "react";
 import Link from "next/link";
 import { FaDev } from "react-icons/fa";
-import { FiSearch } from "react-icons/fi";
 
-import { SearchBox, Button } from "..";
+import { Button } from "..";
 
 const EditorHeader = () => {
   return (
-    <header className="flex header">
-      <div className="flex justify-between items-center header-container">
-        <div className="flex items-center header-container-left">
+    <header className="flex article-header">
+      <div className="flex justify-between items-center article-header-container">
+        <div className="flex items-center article-header-container-left">
           <Link href="/" passHref>
-            <div className="header-container-left__logo flex justify-center items-center">
+            <div className="article-header-container-left__logo flex justify-center items-center">
               <FaDev size="2.6rem" />
             </div>
           </Link>
 
-          <div className="header-container-left__searchBox">
-            <SearchBox className="flex search">
-              <Button className="search__button flex justify-center items-center">
-                <FiSearch />
-              </Button>
-            </SearchBox>
-          </div>
+          <div className="article-header-container-left__head">create post</div>
         </div>
 
-        <div className="flex items-center header-container-right">
-          <div className="login-btn">Login</div>
-          <Button className="btn post-btn">Create Account</Button>
+        <div className="flex items-center article-header-container-right">
+          <div className="article-edit">Edit</div>
+          <div className="article-preview">Preview</div>
         </div>
       </div>
     </header>
