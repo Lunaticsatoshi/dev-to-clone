3f77bfa7430d8c89137e8b75f15c4bc83419d08d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-22 21:28:56 +0530

    :sparkles: Created article feed module

---
+++ client/src/modules/feed/index.tsx
@@ -0,0 +1,5 @@
+const ArticleFeed = () => {
+  return <div className="main-container">Article Feed</div>;
+};
+
+export default ArticleFeed;
--- client/src/modules/index.tsx
+++ client/src/modules/index.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "./sidebar";
 import ListingBar from "./listingbar";
+import ArticleFeed from "./feed";
 import { LoginForm, RegisterForm } from "./auth";
 
-export { LeftSidebar, LoginForm, RegisterForm, ListingBar };
+export { LeftSidebar, LoginForm, RegisterForm, ListingBar, ArticleFeed };
