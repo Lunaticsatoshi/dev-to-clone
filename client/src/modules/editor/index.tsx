7b8790bbe6ae2a8f231448548c5d20e328b55b67
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-22 21:41:04 +0530

    :sparkles: Created editor module

---
+++ client/src/modules/editor/index.tsx
@@ -0,0 +1,5 @@
+const ArticleEditor = () => {
+  return <div className="main-container">Article Editor</div>;
+};
+
+export default ArticleEditor;
--- client/src/modules/index.tsx
+++ client/src/modules/index.tsx
@@ -1,6 +1,14 @@
 import LeftSidebar from "./sidebar";
 import ListingBar from "./listingbar";
 import ArticleFeed from "./feed";
+import ArticleEditor from "./editor";
 import { LoginForm, RegisterForm } from "./auth";
 
-export { LeftSidebar, LoginForm, RegisterForm, ListingBar, ArticleFeed };
+export {
+  LeftSidebar,
+  LoginForm,
+  RegisterForm,
+  ListingBar,
+  ArticleFeed,
+  ArticleEditor,
+};
