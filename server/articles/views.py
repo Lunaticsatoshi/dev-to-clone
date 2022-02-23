b63458f84f1d380e920b95b2753116dfce4d0f94
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-23 22:20:11 +0530

    :gear: Updating article view

--- server/articles/views.py
+++ server/articles/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from drf_yasg.utils import swagger_auto_schema
 
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveDestroyAPIView, GenericAPIView
+from rest_framework.generics import ListAPIView, RetrieveDestroyAPIView, GenericAPIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
