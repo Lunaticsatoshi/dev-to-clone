9d9f6c57f38d2ae228ebbe27e8dd226fa793afc0
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-23 22:18:45 +0530

    :railway_track: Adding update article method to urls

--- server/articles/urls.py
+++ server/articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserArticleListApiView, UserArticleCreateApiView, UserArticleDetailApiView, get_article, get_articles, add_clap, add_comment, edit_comment, delete_comment
+from .views import UserArticleListApiView, UserArticleCreateApiView, UserArticleUpdateApiView, UserArticleDetailApiView, get_article, get_articles, add_clap, add_comment, edit_comment, delete_comment
 
 urlpatterns = [
     path('user/all', UserArticleListApiView.as_view(), name='user-article-list'),
     path('user/create', UserArticleCreateApiView.as_view(), name='user-article-create'),
+    path('user/update/<int:pk>', UserArticleUpdateApiView.as_view(), name='user-article-update'),
     path('user/<int:id>', UserArticleDetailApiView.as_view(), name='user-article-detail'),
     path('all/', get_articles, name='all-articles'),
     path('clap/', add_clap, name='add-clap'),
