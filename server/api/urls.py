150a1577720ad678b1f31fc3cf5c0a14647dcc9b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-24 21:35:22 +0530

    :railway_track: Adding communities urls to base api

--- server/api/urls.py
+++ server/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('user/', include('user.urls')),
     path('articles/', include('articles.urls')),
+    path('communities/', include('communities.urls')),
 ]
