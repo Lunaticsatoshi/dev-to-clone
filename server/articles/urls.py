e95dea63e4addd399a976ea528dd768c1a329ce5
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-24 20:12:25 +0530

    :railway_track: Updating edit article urls

--- server/articles/urls.py
+++ server/articles/urls.py
@@ -4,12 +4,12 @@ from .views import UserArticleListApiView, UserArticleCreateApiView, UserArticle
 urlpatterns = [
     path('user/all', UserArticleListApiView.as_view(), name='user-article-list'),
     path('user/create', UserArticleCreateApiView.as_view(), name='user-article-create'),
-    path('user/update/<int:pk>', UserArticleUpdateApiView.as_view(), name='user-article-update'),
-    path('user/<int:id>', UserArticleDetailApiView.as_view(), name='user-article-detail'),
+    path('user/<str:id>/update/', UserArticleUpdateApiView.as_view(), name='user-article-update'),
+    path('user/<str:id>', UserArticleDetailApiView.as_view(), name='user-article-detail'),
     path('all/', get_articles, name='all-articles'),
     path('clap/', add_clap, name='add-clap'),
     path('comment/', add_comment, name='add-comment'),
     path('<str:slug>/', get_article, name='article-detail'),
-    path('comment/<str:pk>/edit', edit_comment, name='edit-comment'),
-    path('comment/<str:pk>/delete', delete_comment, name='delete-comment'),
+    path('comment/<str:pk>/edit/', edit_comment, name='edit-comment'),
+    path('comment/<str:pk>/delete/', delete_comment, name='delete-comment'),
 ]
