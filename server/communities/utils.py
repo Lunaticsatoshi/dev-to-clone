70da48218ea729409ab8d3e24964d34226ce8207
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-25 21:38:01 +0530

    :zap: Adding generate slug utility

---
+++ server/communities/utils.py
@@ -0,0 +1,8 @@
+from django.utils.crypto import get_random_string
+class Utils():
+    
+    @staticmethod
+    def generate_slug(title):
+        random_str = get_random_string(length=5)
+        slug = title.replace(' ', '-').lower()
+        return f"{slug}-{random_str}"
