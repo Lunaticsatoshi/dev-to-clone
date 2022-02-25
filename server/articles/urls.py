f8dd87e1f5e4c0aa1d8c067e363ff02d2f1a7a7f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-25 21:43:35 +0530

    :railway_track: Updating article urls

--- server/articles/urls.py
+++ server/articles/urls.py
@@ -3,14 +3,14 @@ from .views import UserArticleListApiView, UserArticleCreateApiView, UserArticle
 
 urlpatterns = [
     path('user/all', UserArticleListApiView.as_view(), name='user-article-list'),
-    path('user/create', UserArticleCreateApiView.as_view(), name='user-article-create'),
-    path('user/<str:id>/update/', UserArticleUpdateApiView.as_view(), name='user-article-update'),
-    path('user/<str:id>/delete/', UserArticleDeleteApiView.as_view(), name='user-article-delete'),
     path('user/<str:id>', UserArticleDetailApiView.as_view(), name='user-article-detail'),
+    path('create', UserArticleCreateApiView.as_view(), name='user-article-create'),
+    path('<str:id>/update/', UserArticleUpdateApiView.as_view(), name='user-article-update'),
+    path('<str:id>/delete/', UserArticleDeleteApiView.as_view(), name='user-article-delete'),
     path('all/', get_articles, name='all-articles'),
     path('clap/', add_clap, name='add-clap'),
     path('comment/', add_comment, name='add-comment'),
     path('<str:slug>/', get_article, name='article-detail'),
-    path('comment/<str:pk>/edit/', edit_comment, name='edit-comment'),
-    path('comment/<str:pk>/delete/', delete_comment, name='delete-comment'),
+    path('comment/<str:id>/update/', edit_comment, name='edit-comment'),
+    path('comment/<str:id>/delete/', delete_comment, name='delete-comment'),
 ]
