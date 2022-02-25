dfa9ba366daf664bf064057e1c05db0fc0c21497
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-25 21:39:42 +0530

    :railway_track: Adding add community view to urls

--- server/communities/urls.py
+++ server/communities/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CommunityListApiView, CommunityDetailApiView
+from .views import CommunityListApiView, CommunityDetailApiView, CommunityCreateApiView
 
 urlpatterns = [
     path('user/all/', CommunityListApiView.as_view(), name='community-list'),
     path('user/<str:id>/', CommunityDetailApiView.as_view(), name='community-detail'),
+    path('user/create/', CommunityCreateApiView.as_view(), name='community-create'),
 ]
