c8e37a2cfee8215283504f3a8dc495f646bac4f3
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-25 21:38:23 +0530

    :gear: Adding add community view

--- server/communities/views.py
+++ server/communities/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Communities
 from .serializers import CommunitiesSerializer
+from .utils import Utils
 
 # Create your views here.
 
@@ -37,6 +38,31 @@ class CommunityDetailApiView(GenericAPIView):
         
         except Communities.DoesNotExist:
             return Response({'message': 'Community does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        
+        except Exception as e:
+            return Response({'message': 'Something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class CommunityCreateApiView(GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CommunitiesSerializer
+
+    def post(self, request):
+        user = request.user
+        data = request.data
+        
+        community_name = data.get('name')
+        community_description = data.get('description')    
+        try:
+            if not community_name or not community_description:
+                return Response({'message': 'Please provide all the required fields'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            community_slug = Utils.generate_slug(community_name)
+            
+            community = Communities.objects.create(name=community_name, slug=community_slug, description=community_description, creator=user)
+            community.save()
+            
+            serializer = CommunitiesSerializer(community, many=False)
+            return Response({ 'message': 'Sucessfully created community', 'data': serializer.data },status=status.HTTP_201_CREATED)
         
         except Exception as e:
             return Response({'message': 'Something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
