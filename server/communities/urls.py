195091b852bcc2106780331eaec3f5a83c32fd0f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-26 20:28:29 +0530

    :railway_track: Adding community views to url paths

--- server/communities/urls.py
+++ server/communities/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import CommunityListApiView, CommunityDetailApiView, CommunityCreateApiView
+from .views import CommunityListApiView, CommunityDetailApiView, CommunityCreateApiView, CommunityUpdateApiView, CommunityDeleteApiView, get_communities, get_community, join_community
 
 urlpatterns = [
     path('user/all/', CommunityListApiView.as_view(), name='community-list'),
     path('user/<str:id>/', CommunityDetailApiView.as_view(), name='community-detail'),
     path('create/', CommunityCreateApiView.as_view(), name='community-create'),
+    path('<str:id>/update/', CommunityUpdateApiView.as_view(), name='community-update'),
+    path('<str:id>/delete/', CommunityDeleteApiView.as_view(), name='community-delete'),
+    path('get/', get_communities, name='get-communities'),
+    path('get/<str:slug>/', get_community, name='get-community'),
+    path('<str:slug>/join/', join_community, name='join-community'),
 ]
