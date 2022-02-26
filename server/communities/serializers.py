b3244daa108e5683116ab4a113cf3fd6c6047f39
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-26 20:04:50 +0530

    :jigsaw: Adding validate method to community serializer

--- server/communities/serializers.py
+++ server/communities/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
+from rest_framework import status
 
 from user.serializers import UserProfileSerializer
 from .models import Communities
+from .utils import Utils
 
 class CommunitiesSerializer(ModelSerializer):
     creator = serializers.SerializerMethodField(read_only=True)
@@ -10,6 +12,17 @@ class CommunitiesSerializer(ModelSerializer):
     class Meta:
         model = Communities
         fields = '__all__'
+        
+    def validate(self, attrs):
+        name = attrs.get('name')
+        
+        if Communities.objects.filter(name=name).exists():
+            raise serializers.ValidationError({ 'message': "Community with this name already exists" }, status.HTTP_400_BAD_REQUEST)
+        
+        if not Utils.validate_name(name):
+            raise serializers.ValidationError({ 'message': "Community name can only contain letters, numbers, spaces and '-'" }, status.HTTP_400_BAD_REQUEST)
+        
+        return attrs
         
     def get_creator(self, obj):
         user = obj.creator.userprofile
