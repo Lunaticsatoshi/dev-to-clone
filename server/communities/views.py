2d91c7b1f7a9a0853dffdc231445712fa22263bc
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-26 19:47:26 +0530

    :gear: Adding community update and delete view

--- server/communities/views.py
+++ server/communities/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Communities
 from .serializers import CommunitiesSerializer
+from .permissions import IsOwner
 from .utils import Utils
 
 # Create your views here.
@@ -63,6 +64,59 @@ class CommunityCreateApiView(GenericAPIView):
             
             serializer = CommunitiesSerializer(community, many=False)
             return Response({ 'message': 'Sucessfully created community', 'data': serializer.data },status=status.HTTP_201_CREATED)
+        
+        except Exception as e:
+            return Response({'message': 'Something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class CommunityUpdateApiView(GenericAPIView):
+    permission_classes = (IsAuthenticated, IsOwner,)
+    serializer_class = CommunitiesSerializer
+    
+    def put(self, request, id):
+        user = request.user
+        data = request.data
+        
+        community_name = data.get('name')
+        community_description = data.get('description')
+        
+        try:
+            if community_name :
+                community_slug = Utils.generate_slug(community_name)
+            
+            community = Communities.objects.get(pk=id)
+            community.name = community_name
+            community.slug = community_slug
+            community.description = community_description
+            community.save()
+            
+            serializer = CommunitiesSerializer(community, many=False)
+            return Response({ 'message': 'Sucessfully updated community', 'data': serializer.data },status=status.HTTP_200_OK)
+        
+        except Communities.DoesNotExist:
+            return Response({'message': 'Community does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        
+        except Exception as e:
+            return Response({'message': 'Something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
+class CommunityDeleteApiView(GenericAPIView):    
+    permission_classes = (IsAuthenticated, IsOwner,)
+    serializer_class = CommunitiesSerializer  
+     
+    def delete(self, request, id):
+        user = request.user
+        try:
+            community = Communities.objects.get(pk=id)
+            
+            if user == community.creator:
+                community.delete()
+                communities = Communities.objects.filter(creator=user)
+                serializer = CommunitiesSerializer(communities, many=True)
+                return Response({ 'message': 'Sucessfully deleted community', 'data': serializer.data },status=status.HTTP_200_OK)
+            
+        
+        except Communities.DoesNotExist:
+            return Response({'message': 'Community does not exist'}, status=status.HTTP_404_NOT_FOUND)
         
         except Exception as e:
             return Response({'message': 'Something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
