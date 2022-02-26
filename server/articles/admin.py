61a14c8c3eba185127721f8c719033a7ab044be8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-26 20:53:17 +0530

    :hammer: Adding apps to admin

--- server/articles/admin.py
+++ server/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Article, ArticleClap, ArticleComment, HashTag
+
 # Register your models here.
+
+admin.site.register(Article)
+admin.site.register(ArticleClap)
+admin.site.register(ArticleComment)
+admin.site.register(HashTag)
