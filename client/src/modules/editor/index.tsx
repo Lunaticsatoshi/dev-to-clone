f688716f1f642e785d6215980ed02e558cfa4847
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-02-27 20:54:55 +0530

    :zap: Adding editor styles

--- client/src/modules/editor/index.tsx
+++ client/src/modules/editor/index.tsx
@@ -3,7 +3,7 @@ import { FaDev } from "react-icons/fa";
 
 const ArticleEditor = () => {
   return (
-    <div className="main-container">
+    <div className="editor-container flex justify-between items-center gap-2">
       <div className="article-editor">
         <div className="editor-header flex justify-between items-center">
           <div className="flex items-center editor-header__left">
@@ -13,9 +13,7 @@ const ArticleEditor = () => {
               </div>
             </Link>
 
-            <div className="editor-header-head">
-              create post
-            </div>
+            <div className="editor-header-head">create post</div>
           </div>
 
           <div className="flex items-center editor-header__right">
@@ -23,7 +21,37 @@ const ArticleEditor = () => {
             <div className="editor-preview">Preview</div>
           </div>
         </div>
-        <div className="article-editor__container"></div>
+        <div className="article-editor__container">
+          <div className="article-editor-top">
+            <div className="article-editor-top__cover flex justify-center items-center">
+              <div>Add a cover image</div>
+              <input type="file" />
+            </div>
+            <div className="article-editor-top__title">
+              <textarea
+                className="text-field"
+                placeholder="New post title here..."
+                autoComplete="off"
+                aria-label="Post-Title"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="tips-container">
+        <h1>Writing a great post title</h1>
+        <ul>
+          <li>
+            Think of your post title as a super short (but compelling!)
+            description — like an overview of the actual post in one short
+            sentence.
+          </li>
+          <li>
+            Use keywords where appropriate to help ensure people can find your
+            post by search.
+          </li>
+        </ul>
       </div>
     </div>
   );
