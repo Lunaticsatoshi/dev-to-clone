8ff55ed1bedda5e4a8319ad8a4f4e227611fa292
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-03 20:45:53 +0530

    :zap: Adding formik to editor module

--- client/src/modules/editor/index.tsx
+++ client/src/modules/editor/index.tsx
@@ -1,48 +1,75 @@
 import Link from "next/link";
+import { Form, useFormik, FormikProvider } from "formik";
 import { FaDev } from "react-icons/fa";
+import { CgShapeHexagon } from "react-icons/cg";
 
-import { MDEditor } from "src/components";
+import { MDEditor, InputTextField, Button } from "src/components";
 
 const ArticleEditor = () => {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      content: "",
+    },
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    },
+  });
   return (
     <div className="editor-container flex justify-between items-center gap-2">
-      <div className="article-editor">
-        <div className="editor-header flex justify-between items-center">
-          <div className="flex items-center editor-header__left">
-            <Link href="/" passHref>
-              <div className="editor-header-logo flex justify-center items-center">
-                <FaDev size="2.6rem" />
+      <FormikProvider value={formik}>
+        <div className="article-editor">
+          <div className="editor-header flex justify-between items-center">
+            <div className="flex items-center editor-header__left">
+              <Link href="/" passHref>
+                <div className="editor-header-logo flex justify-center items-center">
+                  <FaDev size="2.6rem" />
+                </div>
+              </Link>
+
+              <div className="editor-header-head">create post</div>
+            </div>
+
+            <div className="flex items-center editor-header__right">
+              <div className="editor-edit">Edit</div>
+              <div className="editor-preview">Preview</div>
+            </div>
+          </div>
+          <Form>
+            <div className="article-editor__container">
+              <div className="article-editor-top">
+                <div className="article-editor-top__cover flex justify-center items-center">
+                  <div>Add a cover image</div>
+                  <input type="file" />
+                </div>
+                <div className="article-editor-top__title">
+                  <InputTextField
+                    name="title"
+                    className="text-field"
+                    placeholder="New post title here..."
+                    autoComplete="off"
+                    aria-label="Post-Title"
+                  />
+                </div>
               </div>
-            </Link>
-
-            <div className="editor-header-head">create post</div>
-          </div>
-
-          <div className="flex items-center editor-header__right">
-            <div className="editor-edit">Edit</div>
-            <div className="editor-preview">Preview</div>
-          </div>
-        </div>
-        <div className="article-editor__container">
-          <div className="article-editor-top">
-            <div className="article-editor-top__cover flex justify-center items-center">
-              <div>Add a cover image</div>
-              <input type="file" />
+              <div className="article-editor-body">
+                <MDEditor name="content" />
+              </div>
             </div>
-            <div className="article-editor-top__title">
-              <textarea
-                className="text-field"
-                placeholder="New post title here..."
-                autoComplete="off"
-                aria-label="Post-Title"
-              />
+            <div className="flex justify-start items-center article-actions">
+              <Button className="btn publish-button">Publish</Button>
+              <div className="draft-button">Save draft</div>
+              <div className="publish-options">
+                <i>
+                  <CgShapeHexagon />
+                </i>
+              </div>
+              <div className="reset-button">Revert new changes</div>
             </div>
-          </div>
-          <div className="article-editor-body">
-            <MDEditor />
-          </div>
+          </Form>
         </div>
-      </div>
+      </FormikProvider>
 
       <div className="tips-container">
         <h1>Writing a great post title</h1>
