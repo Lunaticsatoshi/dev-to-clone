15bcc914b9f3a28374b900e92e108e7512aafce8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-04 20:54:14 +0530

    :jigsaw: Removing validation from community serializer

--- server/communities/serializers.py
+++ server/communities/serializers.py
@@ -13,17 +13,6 @@ class CommunitiesSerializer(ModelSerializer):
         model = Communities
         fields = '__all__'
         
-    def validate(self, attrs):
-        name = attrs.get('name')
-        
-        if Communities.objects.filter(name=name).exists():
-            raise serializers.ValidationError({ 'message': "Community with this name already exists" }, status.HTTP_400_BAD_REQUEST)
-        
-        if not Utils.validate_name(name):
-            raise serializers.ValidationError({ 'message': "Community name can only contain letters, numbers, spaces and '-'" }, status.HTTP_400_BAD_REQUEST)
-        
-        return attrs
-        
     def get_creator(self, obj):
         user = obj.creator.userprofile
         serializer = UserProfileSerializer(user, many=False)
