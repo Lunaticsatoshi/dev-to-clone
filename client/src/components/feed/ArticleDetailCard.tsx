18a854c811f8dcd5ab0cc8357611701022064967
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-07 22:00:46 +0530

    :lipstick: Adding styles for article detail card

--- client/src/components/feed/ArticleDetailCard.tsx
+++ client/src/components/feed/ArticleDetailCard.tsx
@@ -6,7 +6,20 @@ type ArticleDetailProps = {
 };
 
 const ArticleDetailCard: FC<ArticleDetailProps> = ({}) => {
-  return <div className="article-detail-card">ArticleDetailCard</div>;
+  return (
+    <div className="article-detail-card">
+      <a
+        href="/"
+        className="article-detail-card__image"
+        style={{
+          backgroundImage:
+            "url(https://res.cloudinary.com/practicaldev/image/fetch/s--jyXrn67N--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ts3q02co540scso4asc7.png)",
+        }}
+      >
+        &nbsp;
+      </a>
+    </div>
+  );
 };
 
 export default ArticleDetailCard;
