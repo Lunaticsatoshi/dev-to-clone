fcdfcd590a216174b6cba0854e286f60245bb6cf
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-10 20:38:32 +0530

    :lipstick: Styling Article body profile section

--- client/src/components/feed/ArticleDetailCard.tsx
+++ client/src/components/feed/ArticleDetailCard.tsx
@@ -7,17 +7,41 @@ type ArticleDetailProps = {
 
 const ArticleDetailCard: FC<ArticleDetailProps> = ({}) => {
   return (
-    <div className="article-detail-card">
-      <a
-        href="/"
-        className="article-detail-card__image"
-        style={{
-          backgroundImage:
-            "url(https://res.cloudinary.com/practicaldev/image/fetch/s--jyXrn67N--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ts3q02co540scso4asc7.png)",
-        }}
-      >
-        &nbsp;
-      </a>
+    <div className="article-detail-card flex justify-start items-center flex-col">
+      <div className="article-detail-card-top">
+        <a
+          href="/"
+          className="article-detail-card-top__image"
+          style={{
+            backgroundImage:
+              "url(https://res.cloudinary.com/practicaldev/image/fetch/s--jyXrn67N--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ts3q02co540scso4asc7.png)",
+          }}
+        >
+          &nbsp;
+        </a>
+      </div>
+      <div className="article-detail-card-body flex justify-start items-center flex-col">
+        <div className="article-detail-card-body__profile flex justify-start items-center">
+          <div className="profile-image-container">
+            <a
+              href="/"
+              className="profile-image-container__image"
+              style={{
+                backgroundImage:
+                  "url(https://res.cloudinary.com/practicaldev/image/fetch/s--jyXrn67N--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ts3q02co540scso4asc7.png)",
+              }}
+            >
+              &nbsp;
+            </a>
+          </div>
+          <div className="profile-info flex justify-start flex-col">
+            <h1>Jason Taylor</h1>
+            <div>
+              <p>Posted on Mar 7</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
