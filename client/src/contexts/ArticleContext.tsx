ba2786bd03496cf2a66851cff531a118871bf3e8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-11 21:41:02 +0530

    :zap: Adding article reducer to article context

--- client/src/contexts/ArticleContext.tsx
+++ client/src/contexts/ArticleContext.tsx
@@ -6,26 +6,29 @@ import React, {
   useReducer,
 } from "react";
 
+import { articleReducer, ArticleState, ArticleAction } from "src/reducers";
+
 type Props = {
   children: ReactNode;
 };
 
 const initialState = {
-    article: {},
-    articles: [],
-    comments: [],
+  article: {},
+  comment: {},
+  articles: [],
+  comments: [],
 };
 
 export const ArticleContext = createContext<{
-  state: any;
-  dispatch: Dispatch<any>;
+  state: ArticleState;
+  dispatch: Dispatch<ArticleAction>;
 }>({
   state: initialState,
   dispatch: () => null,
 });
 
 const ArticleContextProvider: FC<Props> = ({ children }) => {
-  const [state, dispatch] = useReducer("", initialState);
+  const [state, dispatch] = useReducer(articleReducer, initialState);
   return (
     <ArticleContext.Provider value={{ state, dispatch }}>
       {children}
