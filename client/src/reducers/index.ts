3cfe6e9e13cd4dc401463aed6cef321d6439771e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-11 21:38:13 +0530

    :zap: Adding article user to common export

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,5 +1,13 @@
 import { sidebarReducer, SidebarAction, SidebarState } from "./sidebarReducer";
 import { authReducer, AuthAction, AuthState } from "./authReducer";
+import { articleReducer, ArticleAction, ArticleState } from "./articleReducer";
 
-export { sidebarReducer, authReducer };
-export type { SidebarAction, SidebarState, AuthAction, AuthState };
+export { sidebarReducer, authReducer, articleReducer };
+export type {
+  SidebarAction,
+  SidebarState,
+  AuthAction,
+  AuthState,
+  ArticleAction,
+  ArticleState,
+};
