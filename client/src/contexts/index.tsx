9bfb1129cb62518aa0a9addcbd6139a81c454b5b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-13 20:36:35 +0530

    :zap: Adding article context common export

--- client/src/contexts/index.tsx
+++ client/src/contexts/index.tsx
@@ -1,9 +1,13 @@
 import SidebarContextProvider, { SidebarContext } from "./SidebarContext";
 import AuthContextProvider, { AuthContext } from "./AuthContext";
 
+import ArticleContextProvider, { ArticleContext } from "./ArticleContext";
+
 export {
   SidebarContext,
   SidebarContextProvider,
   AuthContext,
   AuthContextProvider,
+  ArticleContext,
+  ArticleContextProvider
 };
