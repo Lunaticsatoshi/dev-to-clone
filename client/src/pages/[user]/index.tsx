27dbff54274f97a54ac11a916e4fabfadb9640ba
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-15 21:07:35 +0530

    :sparkles: Created user profile page

---
+++ client/src/pages/[user]/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+import { Layout } from "src/components";
+
+const UserProfilePage: NextPage = () => {
+  return (
+    <Layout title="User profile">
+        User
+    </Layout>
+  );
+};
+
+export default UserProfilePage;
