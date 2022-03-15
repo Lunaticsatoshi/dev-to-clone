85ec1ac17e5cec3ddd82ed7ba5c2cd0875d76e1c
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-15 21:16:12 +0530

    :zap: Adding user profile to profile page

--- client/src/pages/[user]/index.tsx
+++ client/src/pages/[user]/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from "next";
 
+import { UserProfile } from "src/modules";
 import { Layout } from "src/components";
 
 const UserProfilePage: NextPage = () => {
-  return (
-    <Layout title="User profile">
-        User
-    </Layout>
-  );
+  return <Layout title="User profile">
+    <div className="user-profile-container">
+      <UserProfile user={{}}/>
+    </div>
+  </Layout>;
 };
 
 export default UserProfilePage;
