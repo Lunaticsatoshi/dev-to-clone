e98b89a2879f8a29e6bd345fc8e50a63d4ac3dd2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-16 21:30:04 +0530

    :lipstick: Styling user profile card

--- client/src/modules/profile/index.tsx
+++ client/src/modules/profile/index.tsx
@@ -1,4 +1,7 @@
 import { FC } from "react";
+import { FaBirthdayCake, FaGithub } from "react-icons/fa";
+
+import { Button } from "src/components";
 
 type ProfileProps = {
   user: any;
@@ -6,7 +9,7 @@ type ProfileProps = {
 
 const UserProfile: FC<ProfileProps> = ({}) => {
   return (
-    <div className="user-profile-card flex justify-center items-center">
+    <div className="user-profile-card flex justify-start items-center flex-col">
       <div className="user-avatar-container flex justify-center items-center">
         <a
           href="/"
@@ -18,6 +21,42 @@ const UserProfile: FC<ProfileProps> = ({}) => {
         >
           &nbsp;
         </a>
+      </div>
+      <div className="user-profile-actions flex justify-end items-center">
+        <Button className="btn edit-profile-btn">Edit Profile</Button>
+      </div>
+      <div className="user-profile-body flex justify-center items-center flex-col">
+        <div className="user-profile-body__name">
+          <h1>Lunaticsatoshi</h1>
+        </div>
+        <div className="user-profile-body__bio">
+          <p>
+            Software Engineer | Full Stack developer | React and Python Lover |
+            ML/AI Enthusiast |
+          </p>
+        </div>
+        <div className="user-profile-body__data flex justify-center items-center">
+          <div className="flex justify-center items-center date-joined">
+            <FaBirthdayCake size={"1.3rem"} />
+            <div>Joined on Jan 31, 2022</div>
+          </div>
+          <div className="data-github">
+            <FaGithub size={"1.3rem"} />
+          </div>
+        </div>
+      </div>
+
+      <div className="user-profile-border"></div>
+
+      <div className="user-profile-info flex justify-between items-center">
+        <div className="info flex justify-center items-center flex-col">
+          <h5>Education</h5>
+          <div>University of Mumbai</div>
+        </div>
+        <div className="info flex justify-center items-center flex-col">
+          <h5>Work</h5>
+          <div>Yodaplus</div>
+        </div>
       </div>
     </div>
   );
