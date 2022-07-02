e4b931b50b34695dd46a64a936cc427b29b2e4f8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-07-02 18:37:49 +0530

    :zap: Removing AuthLayout component

--- client/src/components/shared/Layout.tsx
+++ client/src/components/shared/Layout.tsx
@@ -1,11 +1,15 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect } from "react";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/router";
+
+import { useAuthState } from "src/hooks";
+import { userTokenPersistence } from "src/utils";
+import { SidebarContextProvider } from "src/contexts";
 
 import Header from "./Header";
 
-import { SidebarContextProvider } from "src/contexts";
 type LayoutProps = {
   title: string;
   description?: string;
@@ -19,6 +23,20 @@ const Layout: FC<LayoutProps> = ({
   keywords,
   children,
 }) => {
+  const { currentUser } = useAuthState();
+  const router = useRouter();
+
+  useEffect(() => {
+    const authTokens = userTokenPersistence.get();
+    if (authTokens) {
+      currentUser();
+
+      if (router.pathname === "/login" || router.pathname === "/register") {
+        router.push("/");
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <Head>
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,15 +3,12 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 
 import { AuthContextProvider } from "src/contexts";
-import { AuthLayout } from "src/components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
       <ThemeProvider>
-        <AuthLayout>
           <Component {...pageProps} />
-        </AuthLayout>
       </ThemeProvider>
     </AuthContextProvider>
   );
