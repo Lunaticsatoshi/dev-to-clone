b9c256e15e7bba1cd5f0312a0698f940025f49ed
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-07-31 20:35:32 +0530

    :sparkles: Adding react query client to app

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import "src/styles/app.scss";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import { AuthContextProvider } from "src/contexts";
+import { queryClient } from "src/client/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthContextProvider>
-      <ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <ThemeProvider>
           <Component {...pageProps} />
-      </ThemeProvider>
-    </AuthContextProvider>
+        </ThemeProvider>
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
